0d74d86ae2a3466d37e42753549aa174cc2b4374
Oleksandr Bardashevskyi <o.bardashevskyi@devlight.io>
2023-04-08 10:37:37 +0300

    WebView

--- GotovoApp/ViewController.swift
+++ GotovoApp/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    let webView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+
+
+        return webView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setConstraints()
+
+        guard let url = URL(string: "https://gotovo-staging.fly.dev/") else { return }
+        webView.load(URLRequest(url: url))
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.webView.evaluateJavaScript("document.body.innerHTML") { result, error in
+                guard let html = result as? String, error == nil else {
+                    return
+                }
+
+                print(html)
+            }
+        }
     }
 
+    private func setConstraints() {
+        view.addSubview(webView)
+
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
 }
 
